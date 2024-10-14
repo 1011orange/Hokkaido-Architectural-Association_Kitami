448388705f478f3981f0459c6516996f879903a5
Toichi Hiratsuka <1101orangetennis@gmail.com>
2024-10-14 14:57:05 +0900

    ニュース一覧のPDFをタイトルを編集

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ $('.slider').slick({
 $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
 $('.slider').slick('slickPlay');
 });
+
+
+//トップへのスクロールボタンの記述
+//要素を取得
+const button = document.querySelector('.js-backToTop');
+
+//クリックイベント
+button.addEventListener('click', () => {
+  //ページ上部へスムーススクロール
+  window.scroll({ 
+    top: 0, 
+    behavior: "smooth"
+  });
+});
+
+//スクロールイベント
+window.addEventListener('scroll', () => {
+  //スクロール量を判定して要素にクラスを付与
+  if(window.scrollY > 100){
+    button.classList.add('is-active');
+  }else{
+    button.classList.remove('is-active');
+  }
+});
